cd8422ed2e39abdb84c93fe08696eb38b9fe216b
Rafael Mamatkhodjaev <rawoline@gmail.com>
2024-09-25 14:30:11 +0300

    questions board

--- src/components/QuestionBoard.svelte
+++ src/components/QuestionBoard.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
-  import { fade, fly } from 'svelte/transition';
+  import oktech from "../assets/oktech-logo.svg";
 
-  export let categories: { name: string; questions: { points: number; selected: boolean }[] }[];
+  export let categories: {
+    name: string;
+    questions: { points: number; selected: boolean }[];
+  }[];
   export let onQuestionSelected: (question: string, points: number) => void;
 
   function selectQuestion(categoryIndex: number, questionIndex: number) {
@@ -9,41 +12,149 @@
     if (!question.selected) {
       question.selected = true;
       console.log(question);
-      onQuestionSelected(`Вопрос из категории ${categories[categoryIndex]}`, question.points);
+      onQuestionSelected(
+        `Вопрос из категории ${categories[categoryIndex]}`,
+        question.points
+      );
     }
   }
 </script>
 
+<div class="board">
+  <img src={oktech} class="logo" alt="logo" />
+  <div class="bg1"></div>
+  <div class="bg2"></div>
+  {#each categories as category, i}
+    <div class="category">
+      <h3>{category.name}</h3>
+      <div class="questions">
+        {#each category.questions as question, j}
+          <div
+            class="question {question.selected ? 'selected' : ''}"
+            on:click={() => selectQuestion(i, j)}
+          >
+            {"+" + question.points}
+          </div>
+        {/each}
+      </div>
+    </div>
+  {/each}
+</div>
+
 <style>
+  h3 {
+    display: block;
+    font-size: 40px;
+    font-weight: 600;
+  }
   .board {
-    display: grid;
-    grid-template-columns: repeat(5, 1fr);
+    margin-top: 180px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+  }
+
+  .category {
+    display: flex;
+    width: 90%;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .questions {
+    display: flex;
+    width: 80%;
     gap: 10px;
+    font-size: 40px;
+    font-weight: 600;
   }
 
   .question {
+    width: 18%;
+    height: 100px;
+    border-radius: 50px;
     padding: 20px;
     background: black;
     color: white;
     text-align: center;
     cursor: pointer;
+    transition: all 0.3s;
+  }
+
+  .question:hover {
+    background-color: #fff;
+    color: black;
+  }
+
+  .question:active {
+    background-color: #ff7700;
+    color: white;
   }
 
   .question.selected {
-    background: grey;
+    background: rgba(128, 128, 128, 0.358);
+    color: #bababa49;
     cursor: not-allowed;
   }
-</style>
 
-<div class="board">
-  {#each categories as category, i}
-    <div class="category">
-      <h3>{category.name}</h3>
-      {#each category.questions as question, j}
-        <div class="question {question.selected ? 'selected' : ''}" on:click={() => selectQuestion(i, j)}>
-          {question.selected ? '---' : '+' + question.points}
-        </div>
-      {/each}
-    </div>
-  {/each}
-</div>
+  .bg1 {
+    background-image: url("../assets/bg1.jpg");
+    z-index: -1;
+    animation: fade1 25s infinite;
+  }
+
+  /* Второе изображение начнет с прозрачности */
+  .bg2 {
+    background-image: url("../assets/bg2.jpg");
+    z-index: -2;
+    animation: fade2 25s infinite;
+  }
+
+  .bg1,
+  .bg2 {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-size: cover;
+    background-position: center;
+  }
+
+  .logo {
+    position: absolute;
+    right: 32px;
+    top: 32px;
+  }
+
+  @keyframes fade1 {
+    0% {
+      opacity: 1;
+    }
+
+    50% {
+      opacity: 0;
+    }
+
+    100% {
+      opacity: 1;
+    }
+  }
+
+  /* Анимация плавного перехода */
+  @keyframes fade2 {
+    0% {
+      opacity: 0;
+    }
+
+    50% {
+      opacity: 1;
+    }
+
+    100% {
+      opacity: 0;
+    }
+  }
+</style>
